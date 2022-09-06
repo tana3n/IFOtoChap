4e77ad1e1af2068c55c0076f804db1947ab5fd62
tana3n <tana3n@gmail.com>
2022-09-06 22:51:59 +0900

    IFOtoChap: Fix calcurating time from frame number

--- IFOtoChap.cpp
+++ IFOtoChap.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 
+#include <regex>
 #include <bitset>
 #include <string>
 #include <filesystem>
@@ -18,6 +19,7 @@ std::string appname = "IfoDumpCell";
 std::string Version = "1.0.0";
 
 void usage() {
+    std::cout << appname << " Version " << Version << "\n\n";
     std::cout << "Usage: " + appname + "[options] <input> \n";
 
     std::cout << "--chapter \ttOutput Nero's Chapter Sheet.\n";
@@ -71,70 +73,58 @@ void parse_ifo(const char* input_src, struct _opts *option) {
         std::cout << "NotFound DVDVIDEO-VTS header" << input_src;
         return;
     }
-    std::cout << "PGCN's count:" << uint16_t(iBuf[0x1000]) << uint16_t(iBuf[0x1001]) << std::endl;
+    //std::cout << "PGCN's count:" << uint16_t(iBuf[0x1000]) << uint16_t(iBuf[0x1001]) << std::endl;
     uint16_t PGCNs = iBuf[0x1001];
     uint32_t endAddress = (iBuf[0x1004] << 24) & 0xFFFFFFFF | (iBuf[0x1005] << 16) & 0xFFFFFF | (iBuf[0x1006] << 8) & 0xFFFF | (iBuf[0x1007] & 0xFF);
-
+    /*
     std::cout << "PGCN EndAddress:" 
         << std::hex << endAddress
         << std::endl;
+    */
     uint32_t current = 0x1008;
     uint16_t i = 0;
 
     while (i < PGCNs){
         uint32_t Offsets = ( (iBuf[current + 4] & 0xFF) << 24) | ( (iBuf[current + 5] & 0xFF) << 16) | ( (iBuf[current + 6] & 0xFF) << 8) | ( (iBuf[current + 7] & 0xFF) );
+        /*
         std::cout << "PGCN[" << i + 1 << "]StartByte: 0x"
             << std::hex << Offsets 
             << std::endl;
+        */
         uint32_t PGCN_Offset = Offsets + 0x1000;
         uint16_t PGCN_Cells = iBuf[PGCN_Offset + 0x03];
-        std::cout << "PGCN_Cells count:" << std::dec << PGCN_Cells << std::endl;
+        //std::cout << "PGCN_Cells count:" << std::dec << PGCN_Cells << std::endl;
 
         uint32_t pcgPlayback_offset =( ( (iBuf[PGCN_Offset + 0xE8] & 0xFF) << 8) | (iBuf[PGCN_Offset + 0xE9]&0xFF)) + PGCN_Offset;
 
         //ここでPGC単位のParseをして書き出すと良い
-        std::cout << "cell playback information table[" << i + 1 << "] StartByte: 0x"
+        /*std::cout << "cell playback information table[" << i + 1 << "] StartByte: 0x"
             << std::hex << pcgPlayback_offset
             << std::endl;
+        */
         uint16_t j = 0;
         double playback_ms = 0;
         uint32_t fN = 0;
         if (PGCN_Cells > 1) writeInitial(option);
-        else  std::cout << "[Parser]This PGCN cannot find chapter infomation" << std::endl;
-        while (j < PGCN_Cells - 1) {
+        else;
+
+        while (j < PGCN_Cells) {
             uint32_t tmp_hh = hex2dec(iBuf[pcgPlayback_offset + 4]);
             uint32_t tmp_mm = hex2dec(iBuf[pcgPlayback_offset + 5]);
             uint32_t tmp_ss = hex2dec(iBuf[pcgPlayback_offset + 6]);
             uint32_t tmp_ms = iBuf[pcgPlayback_offset + 7];
             uint32_t frameNum = tmp_ms & 0x3F;
             uint32_t playback_ff = tmp_ms >> 6 & 0x3;
-            //playback_time += (tmp_hh * 60 * 60 + tmp_mm * 60 + tmp_ss) * 1000 + tmp_ms;
 
             uint32_t tM = 60 * tmp_hh + tmp_mm;
             fN =fN + (60 * 60 * 30 * tmp_hh) + (60 * 30 * tmp_mm) + (30 * tmp_ss) + hex2dec(frameNum) -(2 * (tM - tM / 10));
-            
-            uint32_t D = fN / 17982;
 
-            uint32_t M = fN % 17982;
-            uint32_t L = 0;
-            
-            if (M >= 2) L = (M - 2) / 1798;
-            uint32_t fN2 = fN -(D * 18) - (2 * L);
-            //std::cout << std::dec << "fN: " << fN<< "\n";
-//            std::cout << std::setfill('0') << std::right << std::setw(2) << std::dec << "CHAPTER" << j <<"=" << double(fN) * 1001.0 /30000.0<< std::endl;
-            /*
-            playback_hh += playback_time / 1000 / 60 / 60 ;// hh
-            playback_mm += (fN) * 1001.0 / 30000.0 % 60;
-            playback_ss += fN * 1001.0 / 30000.0 % 60;
-            playback_ms += uint32_t(playback_time / 1000) % 60;
-            */
-            //std::cout << "fN2: " << fN2 << "\n";
-            info.playback_ss = uint32_t(double(fN2) * 1001.0 / 30000.0) % 60;
-            info.playback_hh = uint32_t(double(fN2) * 1001.0 / 30000.0) / 60 / 60;
-            info.playback_mm = uint32_t(double(fN2) * 1001.0 / 30000.0) / 60 % 60;
-            info.playback_ms = double(fN2) * 1001.0 / 30000.0 - uint32_t(double(fN2) * 1001.0 / 30000.0);
+            info.playback_ss = uint32_t(double(fN) * 1001.0 / 30000.0) % 60;
+            info.playback_hh = uint32_t(double(fN) * 1001.0 / 30000.0) / 60 / 60;
+            info.playback_mm = uint32_t(double(fN) * 1001.0 / 30000.0) / 60 % 60;
+            info.playback_ms = double(fN) * 1001.0 / 30000.0 - uint32_t(double(fN) * 1001.0 / 30000.0);
             info.playback_no = j + 2;
-            writeChapter(fN2, option, &info);
+            writeChapter(fN, option, &info);
 
             j = j + 1;
             pcgPlayback_offset = pcgPlayback_offset + 24;
@@ -177,6 +167,7 @@ void writeChapter(double playback, struct _opts* option, struct _info* info) {
 }
 
 void writeInitial(struct _opts* option) {
+
     if (option->chapter) {
         std::cout << "CHAPTER01=00:00:00:000" << std::endl;
         std::cout << "CHAPTERNAME01=Chapter 1" << std::endl;
@@ -191,10 +182,9 @@ void writeInitial(struct _opts* option) {
         std::cout << "    TITLE \"Track 01\"" << std::endl;
         std::cout << "    INDEX 01 00:00:00" << std::endl;
     }
-
 }
+
 int main(int argc, char* argv[]) {
-    std::cout << appname <<" Version " << Version << "\n\n";
 
     if (!argv[1]) {
         usage();
